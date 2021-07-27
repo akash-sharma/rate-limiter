0649cedab1d884f2cc2fcf5b8cfec0075b06d9eb
Akash Sharma <akash777.sharma@gmail.com>
2021-07-27 19:20:11 +0530

    Update RlValidator.lua

--- src/main/resources/RlValidator.lua
+++ src/main/resources/RlValidator.lua
@@ -1,23 +1,27 @@
 local function rlAllowed(rlConf)
 
     local rlThresholds = rlConf.rlThresholds
-    local shouldAllow = 1
+    local passRateLimiter = 1
 
     for i = 1, table.getn(rlThresholds) do
 
+        -- Removes all elements in the sorted set stored at key with a score , O(log N)
         redis.call('ZREMRANGEBYSCORE', rlThresholds[i].key, '-inf', rlThresholds[i].lastWindowTime)
+        
+        -- Returns number of elements in a sorted set, O(1)
         local countOfElements = redis.call('ZCARD', rlThresholds[i].key)
 
-        local result = countOfElements < tonumber(rlThresholds[i].threshold)
-        if (result == false) then
-            -- store the hit in redis, even if false
-            shouldAllow = 0
+        local rateLimiterResult = countOfElements < tonumber(rlThresholds[i].threshold)
+        if (rateLimiterResult == false) then
+            -- add key to redis, even if false
+            passRateLimiter = 0
         end
 
+        -- add new element in sorted set, O(log N)
         redis.call('ZADD', rlThresholds[i].key, rlThresholds[i].epoch, rlThresholds[i].epoch)
     end
 
-    return shouldAllow
+    return passRateLimiter
 end
 
 local rlConf = cjson.decode(ARGV[1])
