1e16195be6cbce03eb643be865e4aa61074c8d57
Akash Sharma <akash777.sharma@gmail.com>
2021-07-28 22:54:00 +0530

    Update Route.java

--- src/main/java/com/akash/constant/Route.java
+++ src/main/java/com/akash/constant/Route.java
@@ -2,7 +2,7 @@ package com.akash.constant;
 
 public class Route {
 
-  public static final String V1_PRODUCT = "/v1/product/";
-  public static final String V2_PRODUCT = "/v2/product/";
-  public static final String V3_PRODUCT = "/v3/product/";
+  public static final String V1_PRODUCT = "/v1/product";
+  public static final String V2_PRODUCT = "/v2/product";
+  public static final String V3_PRODUCT = "/v3/product";
 }
