fee65786987b0d0884e223a6972cebdaeab0945a
Akash Sharma <akash777.sharma@gmail.com>
2021-07-28 22:56:40 +0530

    Update RateLimiterService.java

--- src/main/java/com/akash/service/RateLimiterService.java
+++ src/main/java/com/akash/service/RateLimiterService.java
@@ -4,6 +4,8 @@ import com.akash.dto.RLConfig;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import redis.clients.jedis.HostAndPort;
@@ -20,6 +22,8 @@ import java.util.stream.Collectors;
 
 @Service
 public class RateLimiterService {
+
+  private static final Logger LOGGER = LogManager.getLogger(RateLimiterService.class);
 
   @Value("${redisHosts:localhost:6379}")
   private String redisHosts;
@@ -39,6 +43,7 @@ public class RateLimiterService {
       jedisNodes.add(new HostAndPort(hostAndPort[0], Integer.parseInt(hostAndPort[1])));
     }
     jedisClient = new JedisCluster(jedisNodes, new GenericObjectPoolConfig());
+    LOGGER.info("jedisClient : {}", jedisClient);
 
     // load lua script
     InputStream inputStream =
@@ -46,22 +51,28 @@ public class RateLimiterService {
     if (inputStream != null) {
       BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
       luaScript = (String) reader.lines().collect(Collectors.joining(System.lineSeparator()));
+      LOGGER.info("luaScript : {}", luaScript);
     } else {
       throw new RuntimeException("Unable to load lua script");
     }
   }
 
+  // when key for lua script is "{key}mykey", then key is used
+  // for finding single node in a cluster by using its hash
+  // https://stackoverflow.com/questions/49622787/lua-script-attempted-to-access-a-non-local-key-in-a-cluster-node
   public boolean isAllowed(RLConfig rlConfig) throws JsonProcessingException {
 
     if (rlConfig != null) {
       String configJson = MAPPER.writeValueAsString(rlConfig);
-      String shaKey = rlConfig.getClientId();
+      String shaKey = "{" + rlConfig.getClientId() + "}";
+      LOGGER.info("shaKey : {} , configJson : {}", shaKey, configJson);
       Long count =
           (Long)
               jedisClient.eval(
                   luaScript,
                   Collections.singletonList(shaKey),
                   Collections.singletonList(configJson));
+      LOGGER.info("count : {}", count);
       if (count == 1) {
         return true;
       }
